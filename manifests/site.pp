bd179e513a3bf25f6696badf50e88968783e8b64
Rolli, Michael (ID) <michael.rolli@id.unibe.ch>
2017-06-26 10:43:08 +0200

    First sketch for puppetboard.
    For serveral services it could be beneficial to build a reverse proxy on
    gridadmin. Therefore the http port mustn't be open for anybody else
    then.

--- manifests/site.pp
+++ manifests/site.pp
@@ -20,14 +20,37 @@ node 'puppet01.ubelix.unibe.ch' {
 
 node 'puppetdb01.ubelix.unibe.ch' {
   class { 'puppetdb':
-    listen_address       => $puppetdb_cname,
+    #listen_address       => $puppetdb_cname,
     manage_firewall      => true,
-    open_listen_port     => true,
+    open_listen_port     => false,
     open_ssl_listen_port => true,
     java_args            => {
       '-Xmx' => '384m',
       '-Xms' => '128m'
     },
+  }
+
+  # Configure Apache on this server
+  class { 'apache': }
+  class { 'apache::mod::wsgi':
+    wsgi_socket_prefix => "/var/run/wsgi",
+  }
+  # Configure Puppetboard
+  class { 'puppetboard':
+    manage_git        => true,
+    manage_virtualenv => true,
+    reports_count     => 40,
+    listen            => 'public',
+  }
+  # Access Puppetboard through pboard.example.com, port 8888
+  class { 'puppetboard::apache::vhost':
+    vhost_name => 'puppetdb.ubelix.unibe.ch',
+  }
+  firewall { '5000 accept from gridadmin - puppetboard':
+    src_range => '10.1.128.51-10.1.128.52',
+    dport     => 5000,
+    proto     => 'tcp',
+    action    => 'accept',
   }
 }
 
