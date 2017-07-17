1949a4d977930d91358c8fe576bea05067ce4f64
Rolli, Michael (ID) <michael.rolli@id.unibe.ch>
2017-07-17 08:01:38 +0200

    Module refresher script.
    This script is useful in pocket UBELIX only. It makes sure that all
    module listed in Puppetfile of current (prefs.conf) environment are
    installed with respective version in current environment aka modules
    refreshening.

---
+++ refresh_modules.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Source preferences and common functions
+workdir=$(dirname -- $(readlink -f $0))
+source $workdir/shellfunctions.sh
+source $workdir/prefs.conf
+prodenv=$(puppet config print environmentpath)/$ENVIRONMENT
+
+if ! type puppet >/dev/null 2>&1
+then
+  fail "Puppet binary not found."
+  exit 1
+fi
+
+if [ ! -d /vagrant ]
+then
+  fail "Not in a virtualbox machine. Refusing to work."
+  exit 1
+fi
+
+if [ ! -d $prodenv/modules/site ]
+then
+  fail "No puppetenv found in ${prodenv}. Have you installed and run r10k yet?"
+  exit 1
+fi
+
+r10k puppetfile install \
+  --verbose \
+  --puppetfile $prodenv/Puppetfile \
+  --moduledir $prodenv/modules
+
+success "Refreshed modules in ${prodenv}/modules."
+
+exit 0
