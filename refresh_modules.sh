1703e891a792eef72aed131ebb593246dc3dd40e
Rolli, Michael (ID) <michael.rolli@id.unibe.ch>
2018-10-02 17:46:58 +0200

    Script to refresh modules from Puppetfile

--- refresh_modules.sh
+++ refresh_modules.sh
@@ -2,12 +2,45 @@
 
 set -e
 
-# Source preferences and common functions
-workdir=$(dirname -- $(readlink -f $0))
-source $workdir/shellfunctions.sh
-source $workdir/prefs.conf
-prodenv=$(puppet config print environmentpath --section main)/$ENVIRONMENT
+# Settings; change to your needs
+PUP_ENV=development
 
+# General functions for output beautification
+prompt_confirm() {
+  while true; do
+    printf "\r  [ \033[0;33m??\033[0m ] ${1:-Continue?} [y/n]: "
+    read -r -n 1 REPLY
+    case $REPLY in
+      [yY]) echo ; return 0 ;;
+      [nN]) echo ; return 1 ;;
+      *) printf " \033[31m %s \n\033[0m" "invalid input"
+    esac
+  done
+}
+
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user () {
+  printf "\r  [ \033[00;33m??\033[0m ] $1"
+}
+
+warning () {
+  printf "\r  [ \033[00;33m!!\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
+# Checking environment
 if ! type puppet >/dev/null 2>&1
 then
   fail "Puppet binary not found."
@@ -20,17 +53,20 @@ then
   exit 1
 fi
 
-if [ ! -d $prodenv/site ]
+# Setup variables
+envdir=$(puppet config --section main print environmentpath)/$PUP_ENV
+
+if [ ! -d $envdir/site ]
 then
-  fail "No controlrepo found in ${prodenv}. Have you installed and run r10k yet?"
+  fail "No controlrepo found in ${envdir}. Have you installed and run r10k yet?"
   exit 1
 fi
 
 r10k puppetfile install \
   --verbose \
-  --puppetfile $prodenv/Puppetfile \
-  --moduledir $prodenv/modules
+  --puppetfile $envdir/Puppetfile \
+  --moduledir $envdir/modules
 
-success "Refreshed modules in ${prodenv}/modules."
+success "Refreshed modules in ${envdir}/modules. Don't forget to create symlinks if needed."
 
 exit 0
