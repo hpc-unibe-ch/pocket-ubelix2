c004e1b581c68818fdaef8b92ecfd1c223bc25e7
Rolli, Michael (ID) <michael.rolli@id.unibe.ch>
2020-01-28 09:55:10 +0100

    Get rid of settings.inc.sh
    Having some stuff centralized doesn't help much and is annoying for
    setup_puppet-agent.sh, where it has been withdrawn.
    The output beautifier functions are not really changed often. Same goes
    for the other settings. Most of the scripts only use $PUP_ENV or nothing
    at all.

--- create_devsymlinks.sh
+++ create_devsymlinks.sh
@@ -2,9 +2,43 @@
 
 set -e
 
-# Setup variables
-workdir=$(dirname -- $(readlink -f $0))
-source $workdir/settings.inc.sh
+# General settings
+PUP_ENV="development"
+
+# General functions used in shell scripts
+prompt_confirm() {
+  while true; do
+    printf "\r  [ \033[0;33m??\033[0m ] ${1:-Continue?} [y/n]: "
+    read -r -n 1 REPLY
+    case $REPLY in
+      [yY]) echo ; return 0 ;;
+      [nN]) echo ; return 1 ;;
+      *) printf " \033[31m %s \n\033[0m" "invalid input"
+    esac
+  done
+}
+
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user () {
+  printf "\r  [ \033[00;33m??\033[0m ] $1"
+}
+
+warning () {
+  printf "\r  [ \033[00;33m!!\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
 
 if [ ! -f /etc/vagrant_box_build_time ]
 then
